7da869e6d3f20f9c59f4ec60b2e224acbdc79e74
Artefact2 <artefact2@gmail.com>
2012-05-15 10:54:12 +0200

    Update slotcount when subsystems are added.

--- src/json/update_modules.php
+++ src/json/update_modules.php
@@ -25,6 +25,7 @@ if(!osmium_logged_in()) {
 
 
 if(isset($_GET['token']) && $_GET['token'] == osmium_tok()) {
+  $fit = &osmium_get_fit();
   $modules = array(
 		   'high' => array(), 
 		   'medium' => array(), 
@@ -43,7 +44,14 @@ if(isset($_GET['token']) && $_GET['token'] == osmium_tok()) {
     }
   }
 
-  $req = osmium_pg_query_params('SELECT typename, invmodules.typeid, slottype FROM osmium.invmodules JOIN osmium.dgmmoduleattributes ON dgmmoduleattributes.typeid = invmodules.typeid WHERE invmodules.typeid IN ('.
+  list($highslots, $medslots, $lowslots, $rigslots, $subsystemslots) = pg_fetch_row(osmium_pg_query_params('SELECT hislots, medslots, lowslots, rigslots, subsystemslots FROM osmium.dgmshipslots WHERE typeid = $1', array($fit['hull']['typeid'])));
+  $fit['hull']['slotcount']['high'] = $highslots;
+  $fit['hull']['slotcount']['medium'] = $medslots;
+  $fit['hull']['slotcount']['low'] = $lowslots;
+  $fit['hull']['slotcount']['rig'] = $rigslots;
+  $fit['hull']['slotcount']['subsystem'] = $subsystemslots;
+
+  $req = osmium_pg_query_params('SELECT typename, invmodules.typeid, slottype, extrahighslots, extramedslots, extralowslots FROM osmium.invmodules JOIN osmium.dgmmoduleattributes ON dgmmoduleattributes.typeid = invmodules.typeid WHERE invmodules.typeid IN ('.
 				implode(',', array_merge(array(-1), array_keys($typeids))).')', array());
   $rows = array();
   while($row = pg_fetch_row($req)) {
@@ -52,11 +60,13 @@ if(isset($_GET['token']) && $_GET['token'] == osmium_tok()) {
 
   foreach($modules as $type => &$mods) {
     foreach($mods as &$m) {
+      $fit['hull']['slotcount']['high'] += $rows[$m][3];
+      $fit['hull']['slotcount']['medium'] += $rows[$m][4];
+      $fit['hull']['slotcount']['low'] += $rows[$m][5];
       $m = array('typename' => $rows[$m][0], 'typeid' => $rows[$m][1], 'slottype' => $rows[$m][2]);
     }
   }
 
-  $fit = &osmium_get_fit();
   foreach($modules as $type => &$mods) {
     for($i = count($mods); $i < $fit['hull']['slotcount'][$type]; ++$i) {
       $mods[] = -1;
