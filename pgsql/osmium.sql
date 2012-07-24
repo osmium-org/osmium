95848dadf79596c376b38dce9d8bc04fe2c7af9b
Artefact2 <artefact2@gmail.com>
2012-07-24 19:30:02 +0200

    Fix: make sure charge volume <= module capacity in invcharges.

--- pgsql/osmium.sql
+++ pgsql/osmium.sql
@@ -348,7 +348,7 @@ ALTER SEQUENCE flags_flagid_seq OWNED BY flags.flagid;
 --
 
 CREATE VIEW invcharges AS
-    SELECT modattribs.typeid AS moduleid, invtypes.typeid AS chargeid, invtypes.typename AS chargename FROM (((eve.dgmtypeattribs modattribs LEFT JOIN eve.dgmtypeattribs modchargesize ON (((modchargesize.attributeid = 128) AND (modchargesize.typeid = modattribs.typeid)))) JOIN eve.invtypes ON (((modattribs.value)::integer = invtypes.groupid))) LEFT JOIN eve.dgmtypeattribs chargesize ON (((chargesize.attributeid = 128) AND (chargesize.typeid = invtypes.typeid)))) WHERE (((modattribs.attributeid = ANY (ARRAY[604, 605, 606, 609, 610])) AND (((chargesize.value IS NULL) OR (modchargesize.value IS NULL)) OR (chargesize.value = modchargesize.value))) AND (invtypes.published = 1));
+    SELECT modattribs.typeid AS moduleid, invtypes.typeid AS chargeid, invtypes.typename AS chargename FROM ((((eve.dgmtypeattribs modattribs LEFT JOIN eve.dgmtypeattribs modchargesize ON (((modchargesize.attributeid = 128) AND (modchargesize.typeid = modattribs.typeid)))) JOIN eve.invtypes ON (((modattribs.value)::integer = invtypes.groupid))) LEFT JOIN eve.dgmtypeattribs chargesize ON (((chargesize.attributeid = 128) AND (chargesize.typeid = invtypes.typeid)))) JOIN eve.invtypes modcapacity ON ((modcapacity.typeid = modattribs.typeid))) WHERE ((((modattribs.attributeid = ANY (ARRAY[604, 605, 606, 609, 610])) AND (((chargesize.value IS NULL) OR (modchargesize.value IS NULL)) OR (chargesize.value = modchargesize.value))) AND (modcapacity.capacity >= invtypes.volume)) AND (invtypes.published = 1));
 
 
 --
