db0bd4ec40e28273107b1f6f78364179b8009c49
Artefact2 <artefact2@gmail.com>
2012-08-08 00:48:54 +0200

    Fix: embarrassing mistake.

--- src/fatal.php
+++ src/fatal.php
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace Osmium\Page\EditComment;
+namespace Osmium\Page\Fatal;
 
 require __DIR__.'/../inc/root.php';
 
