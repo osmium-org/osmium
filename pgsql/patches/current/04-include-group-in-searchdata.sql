93f5cdea138c0af76af645a8162086d568609790
Artefact2 <artefact2@gmail.com>
2012-10-20 14:17:27 +0200

    Add ship groupname in the search index.
    This allows queries like "shield pvp battlecruiser" or "fleet
    battleship" to return meaningful results.

---
+++ pgsql/patches/current/04-include-group-in-searchdata.sql
@@ -0,0 +1,49 @@
+DROP VIEW loadoutssearchdata;
+
+CREATE OR REPLACE VIEW loadoutssearchdata AS 
+ SELECT searchableloadouts.loadoutid, 
+        CASE loadouts.viewpermission
+            WHEN 4 THEN accounts.accountid
+            ELSE 0
+        END AS restrictedtoaccountid, 
+        CASE loadouts.viewpermission
+            WHEN 3 THEN 
+            CASE accounts.apiverified
+                WHEN true THEN accounts.corporationid
+                ELSE (-1)
+            END
+            ELSE 0
+        END AS restrictedtocorporationid, 
+        CASE loadouts.viewpermission
+            WHEN 2 THEN 
+            CASE accounts.apiverified
+                WHEN true THEN accounts.allianceid
+                ELSE (-1)
+            END
+            ELSE 0
+        END AS restrictedtoallianceid, 
+    fittingaggtags.taglist AS tags, fittingfittedtypes.typelist AS modules, 
+        CASE accounts.apiverified
+            WHEN true THEN accounts.charactername
+            ELSE accounts.nickname
+        END AS author, 
+    fittings.name, fittingdescriptions.descriptions AS description, 
+    fittings.hullid AS shipid, invtypes.typename AS ship, fittings.creationdate, 
+    loadouthistory.updatedate, ls.upvotes, ls.downvotes, ls.score, 
+    invgroups.groupname::text AS groups
+   FROM searchableloadouts
+   JOIN loadoutslatestrevision ON searchableloadouts.loadoutid = loadoutslatestrevision.loadoutid
+   JOIN loadouts ON loadoutslatestrevision.loadoutid = loadouts.loadoutid
+   JOIN accounts ON loadouts.accountid = accounts.accountid
+   JOIN loadouthistory ON loadouthistory.loadoutid = loadoutslatestrevision.loadoutid AND loadouthistory.revision = loadoutslatestrevision.latestrevision
+   JOIN fittings ON fittings.fittinghash = loadouthistory.fittinghash
+   JOIN loadoutscores ls ON ls.loadoutid = searchableloadouts.loadoutid
+   JOIN eve.invtypes ON invtypes.typeid = fittings.hullid
+   LEFT JOIN fittingaggtags ON fittingaggtags.fittinghash = loadouthistory.fittinghash
+   LEFT JOIN fittingfittedtypes ON fittingfittedtypes.fittinghash = loadouthistory.fittinghash
+   LEFT JOIN fittingdescriptions ON fittingdescriptions.fittinghash = loadouthistory.fittinghash
+   LEFT JOIN eve.invgroups ON invgroups.groupid = invtypes.groupid;
+
+ALTER TABLE loadoutssearchdata
+  OWNER TO osmium;
+
