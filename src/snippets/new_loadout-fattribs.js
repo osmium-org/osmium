081047f5201fdb492e342c54a56285ead3d8f2c2
Josiah Boning <jboning@gmail.com>
2013-12-28 11:37:39 -0800

    GUIDELINES complaince (copyright notices and mailmap)

--- src/snippets/new_loadout-fattribs.js
+++ src/snippets/new_loadout-fattribs.js
@@ -1,5 +1,6 @@
 /* Osmium
  * Copyright (C) 2013 Romain "Artefact2" Dalmaso <artefact2@gmail.com>
+ * Copyright (C) 2013 Josiah Boning <jboning@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
