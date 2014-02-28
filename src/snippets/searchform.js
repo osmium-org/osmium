1b695a03ccdf0ffd78025c236644502cf28c4f6a
Artefact2 <artefact2@gmail.com>
2014-02-28 18:32:20 +0100

    Proof of concept: filter loadouts by flyability.
    The code is fairly ugly as it creates one Sphinx attribute per skill,
    but it works.

---
+++ src/snippets/searchform.js
@@ -0,0 +1,28 @@
+/* Osmium
+ * Copyright (C) 2014 Romain "Artefact2" Dalmaso <artefact2@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$(function() {
+	$("div#search_full, div#search_mini").find('input#sr').on('change', function() {
+		var inp = $(this);
+		var select = inp.closest('form').find('select#ss');
+		if(inp.prop('checked')) {
+			select.removeProp('disabled');
+		} else {
+			select.prop('disabled', 'disabled');
+		}
+	}).change();
+});
