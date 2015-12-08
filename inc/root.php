b7f9e0f25535c28ca787fd0c6101fc696f514c57
Artefact2 <artefact2@gmail.com>
2015-12-09 00:22:59 +0100

    Prepare for Frostline.

--- inc/root.php
+++ inc/root.php
@@ -85,7 +85,7 @@ const CSS_STATICVER = 28;
 const JS_STATICVER = 35;
 
 /** Bump this when clientdata.json is updated */
-const CLIENT_DATA_STATICVER = 46;
+const CLIENT_DATA_STATICVER = 47;
 
 define(__NAMESPACE__.'\CACHE_DIRECTORY', ROOT.'/cache');
 
